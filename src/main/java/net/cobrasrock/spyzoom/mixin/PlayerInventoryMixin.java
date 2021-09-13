869a7a4357cd289f274b68517f7ef4a57ab35aa9
Hibiii <69405847+Hibiii@users.noreply.github.com>
2021-09-13 16:25:38 -0300

    Improve readability on PlayerIInventoryMixin

--- src/main/java/net/cobrasrock/spyzoom/mixin/PlayerInventoryMixin.java
+++ src/main/java/net/cobrasrock/spyzoom/mixin/PlayerInventoryMixin.java
@@ -2,6 +2,8 @@ package net.cobrasrock.spyzoom.mixin;
 
 import net.cobrasrock.spyzoom.SpyZoom;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.util.math.MathHelper;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,25 +13,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class PlayerInventoryMixin {
 	@Inject(at = @At("HEAD"), method = "scrollInHotbar", cancellable = true)
 	private void scrollInHotbar(double scrollAmount, CallbackInfo info) {
-		if(SpyZoom.instance.player != null) {
-			if (SpyZoom.instance.player.isUsingSpyglass() && SpyZoom.instance.options.getPerspective().isFirstPerson()) {
-				//zooms in/out
-				if (scrollAmount > 0.0D) {
-					SpyZoom.zoom *= 9.0 /10;
-				}
-				if (scrollAmount < 0.0D) {
-					SpyZoom.zoom *= 10.0 /9;
-				}
-				//prevents zooming too far
-				if(SpyZoom.zoom < 0.001){
-					SpyZoom.zoom = 0.001f;
-				}
-				if(SpyZoom.zoom > 1){
-					SpyZoom.zoom = 1f;
-				}
-
-				info.cancel();
+		if(SpyZoom.instance.player == null) return;
+		if(SpyZoom.instance.player.isUsingSpyglass() && SpyZoom.instance.options.getPerspective().isFirstPerson()) {
+			//zooms in/out
+			if (scrollAmount > 0.0D && SpyZoom.zoom > 0.001f) {
+				SpyZoom.zoom *= 9.0f / 10.0f;
 			}
+			if (scrollAmount < 0.0D && SpyZoom.zoom < 1.0f) {
+				SpyZoom.zoom *= 10.0f / 9f;
+			}
+			//prevents zooming from going OOB
+			SpyZoom.zoom = MathHelper.clamp(SpyZoom.zoom, 0.001f, 1.0f);
+
+			info.cancel();
 		}
 	}
 }
