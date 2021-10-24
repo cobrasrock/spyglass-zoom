f94aa9ceb86818b0c9684ebebeb9bcf60bc1bebe
Tuxinal <24763016+tuxinal@users.noreply.github.com>
2021-10-24 14:34:30 +0330

    Reset zoom when spyglass is let go of

---
+++ src/main/java/net/cobrasrock/spyzoom/mixin/SpyglassItemMixin.java
@@ -0,0 +1,17 @@
+package net.cobrasrock.spyzoom.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.cobrasrock.spyzoom.SpyZoom;
+import net.minecraft.item.SpyglassItem;
+
+@Mixin(SpyglassItem.class)
+public class SpyglassItemMixin {
+  @Inject(method = "onStoppedUsing", at = @At("HEAD"))
+  private void resestZoom(CallbackInfo ci) {
+    SpyZoom.zoom = 0.1f;
+  }
+}
